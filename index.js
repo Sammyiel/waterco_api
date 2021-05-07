5be6cf8ecd93fa59846a965ac60cf56b026f68d4
Sammyiel <s.maisiba@alustudent.com>
2021-05-07 21:32:42 +0200

    users authenticated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
@@ -28,3 +29,24 @@ app.use(router);
 app.listen(port, () => {
     console.log(`Our Book Club API is now available on port ${port}`)
 })
+
+app.post('/login', (req, res) => {
+    const username = req.params.username
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, process.env.AUTH_KEY)
+    res.json({ accessToken: accessToken })
+})
+
+export function authenticate(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.AUTH_KEY);
+        next();
+    } catch (err) {
+        res.status(401).json({
+            status: 'failed',
+            message: "Authentication Failed: Incorrect Auth Token."
+        })
+    }
+}
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,26 +5,26 @@ import { authenticate } from '../middlewares/auth.js';
 const usersRouter = express.Router();
 
 //Add a User - Sign Up - Authenticate.
-usersRouter.post("/", addUser);
+usersRouter.post("/", authenticate, addUser);
 
 //Add a User - Sign In - No need of Authentication
 usersRouter.post("/signin", signIn);
 
 //View a User users/:id - Authenticate.
-usersRouter.get("/:id", viewUser);
+usersRouter.get("/:id", authenticate, viewUser);
 
-usersRouter.get("/email/:email_address", viewUserEmail);
+usersRouter.get("/email/:email_address", authenticate, viewUserEmail);
 
 //View all Users users/ - Authenticate.
-usersRouter.get("/", viewAllUsers);
+usersRouter.get("/", authenticate, viewAllUsers);
 
 // added the following routes
 
 // Edit/update a User
-usersRouter.put("/", updateUser);
+usersRouter.put("/", authenticate, updateUser);
 
 // close/delete a User
-usersRouter.delete("/:id", deleteUser);
+usersRouter.delete("/:id", authenticate, deleteUser);
 
 
 export default usersRouter;
